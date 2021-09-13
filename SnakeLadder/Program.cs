8d92b3eb1ae6bf20ba284a2d22778b727e4990a2
Tejassonar <sonartejas908@gmail.com>
2021-09-13 08:04:05 +0530

    Added snakeladder simulation

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RollDice.Roll();
+        }
+    }
+}
---
+++ SnakeLadder/RollDice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    class RollDice
+    {
+        public static void Roll()
+        {
+            
+            Random random = new Random();
+            int Dice = random.Next(0, 6);
+            
+
+            
+        }
+
+    }
+}
